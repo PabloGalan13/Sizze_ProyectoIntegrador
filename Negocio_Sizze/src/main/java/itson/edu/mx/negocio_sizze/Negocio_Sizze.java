9127092050be88cd27b36ce3c0dbb48dd2ff5efb
Gabriel <140923456+Yohan-Melendrez@users.noreply.github.com>
2025-04-08 22:17:31 -0700

    mejor organizacion

---
+++ Negocio_Sizze/src/main/java/itson/edu/mx/negocio_sizze/Negocio_Sizze.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.edu.mx.negocio_sizze;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Negocio_Sizze {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
